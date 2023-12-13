26b8bd41c56b3b597c665212967d4224713972b6
brunopiato <piatobio@gmail.com>
2023-12-13 17:42:37 -0300

    fixing file path error

--- handler.py
+++ handler.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 import pandas as pd
 from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
@@ -40,5 +41,6 @@ def rossmann_predict():
         return Response('{}', status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    port = os.environ.get('PORT', 5000)
+    app.run('0.0.0.0', port=port)
     
